35c8f4949443fcda54d2d2eafb23ccfebe903254
sylnexai <sylnex.ai@gmail.com>
2026-05-15 03:04:37 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { topic } = req.body;
+   const { topic, platform } = req.body;
 
     const response = await fetch("https://api.openai.com/v1/responses", {
       method: "POST",
